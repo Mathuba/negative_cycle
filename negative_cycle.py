5c5d8c4eefa033734e224d4aded7f86c2cc9faf6
Mathuba <Mathuba58@gmail.com>
2019-06-27 20:08:21 +0200

    Added function add_edge to build graph and read graph values in main

--- negative_cycle.py
+++ negative_cycle.py
@@ -3,21 +3,28 @@
 import sys
 
 
+def add_edge(graph, vert1, vert2, weight):
+    """Build directed graph with edge weights.
+
+    This directed graph may also contain negative weights.
+    """
+
+    weighted_edge = [vert2, weight]
+    if vert1 in graph:
+        graph[vert1].append(weighted_edge)
+    else:
+        graph[vert1] = weighted_edge
+
 def negative_cycle(adj, cost):
     #write your code here
     return 0
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
-    data = data[3 * m:]
-    adj = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
-    for ((a, b), w) in edges:
-        adj[a - 1].append(b - 1)
-        cost[a - 1].append(w)
-    print(negative_cycle(adj, cost))
+    n, m = map(int, sys.stdin.readline().split())
+    graph = {vertex: [] for vertex in range(1, n+1)}
+    for _ in range(m):
+        vert1, vert2, weight = map(int, sys.stdin.readline().split())
+        add_edge(graph, vert1, vert2, weight)
+    # print(negative_cycle(adj, cost))
+    print(graph)
