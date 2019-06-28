cd8d63e4ec415ac58ff3dea7e14b1473b22d11e6
Mathuba <Mathuba58@gmail.com>
2019-06-28 20:15:32 +0200

    Added function to detect negative cycles in graph

--- negative_cycle.py
+++ negative_cycle.py
@@ -3,6 +3,11 @@
 import sys
 
 
+DISTANCE = 0
+VERTEX = 1
+INFINITY = float("inf")
+
+
 def add_edge(graph, vert1, vert2, weight):
     """Build directed graph with edge weights.
 
@@ -15,8 +20,50 @@ def add_edge(graph, vert1, vert2, weight):
     else:
         graph[vert1] = weighted_edge
 
-def negative_cycle(adj, cost):
-    #write your code here
+def negative_cycle(graph, start):
+    """Implement Belman-Ford algorithm
+
+    Will also detect negative cycles.
+
+    Parameters
+    ----------
+    graph: dictionary
+        directed graph with edge weights which may be negative
+
+    Returns
+    -------
+    1: int
+        Return 1 if graph contains a negative cycle
+
+    0: int
+        Return 0 if graph does not contain a negative cycle
+    """
+
+    # initialise storages
+    dist = [INFINITY for i in range((len(graph)+1))]
+    prev = [None for i in range((len(graph)+1))]
+
+    dist[start] = 0
+    prev[start] = start
+    graph_length = len(graph)
+
+    # relax edges
+    for i in range(graph_length):
+        for key_node in graph:
+            for neighbour_node in graph[key_node]:
+                if neighbour_node:
+                    nb_vert, nb_weight = neighbour_node
+                    if dist[nb_vert] > dist[key_node] + nb_weight:
+                        dist[nb_vert] = dist[key_node] + nb_weight
+                        prev[nb_vert] = key_node
+
+    # check for negative cycles
+    for key_node in graph:
+        for neighbour_node in graph[key_node]:
+            if neighbour_node:
+                nb_vert, nb_weight = neighbour_node
+                if dist[nb_vert] > dist[key_node] + nb_weight:
+                    return 1
     return 0
 
 
@@ -26,5 +73,4 @@ if __name__ == '__main__':
     for _ in range(m):
         vert1, vert2, weight = map(int, sys.stdin.readline().split())
         add_edge(graph, vert1, vert2, weight)
-    # print(negative_cycle(adj, cost))
-    print(graph)
+    print(negative_cycle(graph, 1))
